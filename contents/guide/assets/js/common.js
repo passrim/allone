00ce0fcdebdd85b96de37f8414ca397b7ea65f02
passrim <passrim@gmail.com>
2022-04-13 10:08:33 +0900

    초기화

--- contents/guide/assets/js/common.js
+++
@@ -1,8 +0,0 @@
-
-// directiory map tree class append
-$(document).ready(function(){
-	var checked = $('ol li > input');
-	checked.click(function(){
-		$(this).parent('li').toggleClass('treechecked');
-	});
-});
